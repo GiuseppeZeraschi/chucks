e471e02dc744b8cfed703d88889f9026d453efaf
Giuseppe Zeraschi <giuseppezeraschi@gmail.com>
2019-04-16 20:35:43 +0100

    scaffolded contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus error ea qui illum numquam? Autem nihil explicabo aliquam nisi accusamus reiciendis exercitationem ea ipsam sequi error, pariatur expedita quasi tempore eum. Voluptas nostrum culpa rerum facilis adipisci sint vitae necessitatibus! Ut saepe adipisci, veritatis, fugit quas sint aliquam repudiandae eos placeat pariatur voluptatum nihil quaerat dolor, harum perspiciatis eum! Enim ea possimus, facere quod iure molestiae, blanditiis eius nulla mollitia quia quas perspiciatis vitae saepe rerum ex nam animi soluta. Eaque saepe perspiciatis quos quibusdam omnis impedit incidunt repellat ratione, ab eum quia odio explicabo obcaecati a deleniti, aperiam illo!</h1>
+  <div class="v-about v-section">
+    <div class="u-margin-bottom-saturn"></div>
+    <div class="v-contact__contact">
+      <div>
+        <i class="fas fa-phone"></i>
+        <div></div>
+      </div>
+    </div>
   </div>
 </template>
