2322e61dded5af57fb579b703369d0d07bdf73a2
Giuseppe Zeraschi <giuseppezeraschi@gmail.com>
2019-04-20 18:49:01 +0100

    completed form layout and refactored

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,248 @@
+<template>
+  <form class="v-contact-form" method="POST" data-netlify="true">
+    <div class="v-contact-form__input-group">
+      <div class="v-contact-form__input-wrapper">
+        <input
+          @click="firstNameInputActive = true"
+          @blur="firstNameInputActive = false"
+          v-model="firstName"
+          :class="firstNameInputClasses"
+          class="v-contact-form__input v-contact-form__input--text"
+          type="text"
+          name="firstName"
+          id="firstName"
+          required
+        >
+        <label
+          :class="firstNameLabelClasses"
+          class="v-contact-form__label u-text-mars-title"
+          for="firstName"
+        >First Name</label>
+      </div>
+      <div class="v-contact-form__input-wrapper">
+        <input
+          @click="lastNameInputActive = true"
+          @blur="lastNameInputActive = false"
+          v-model="lastName"
+          :class="lastNameInputClasses"
+          class="v-contact-form__input v-contact-form__input--text"
+          type="lastName"
+          name="lastName"
+          id="lastName"
+          required
+        >
+        <label
+          :class="lastNameLabelClasses"
+          class="v-contact-form__label u-text-mars-title"
+          for="lastName"
+        >Last Name</label>
+      </div>
+    </div>
+    <div class="v-contact-form__input-group">
+      <div class="v-contact-form__input-wrapper">
+        <input
+          @click="phoneInputActive = true"
+          @blur="phoneInputActive = false"
+          v-model="phone"
+          :class="phoneInputClasses"
+          class="v-contact-form__input v-contact-form__input--text"
+          type="text"
+          name="phone"
+          id="phone"
+          required
+        >
+        <label
+          :class="phoneLabelClasses"
+          class="v-contact-form__label u-text-mars-title"
+          for="phone"
+        >Phone</label>
+      </div>
+      <div class="v-contact-form__input-wrapper">
+        <input
+          @click="emailInputActive = true"
+          @blur="emailInputActive = false"
+          v-model="email"
+          :class="emailInputClasses"
+          class="v-contact-form__input v-contact-form__input--text"
+          type="email"
+          name="email"
+          id="email"
+          required
+        >
+        <label
+          :class="emailLabelClasses"
+          class="v-contact-form__label u-text-mars-title"
+          for="email"
+        >Email</label>
+      </div>
+    </div>
+    <div class="v-contact-form__input-wrapper">
+      <textarea
+        @click="messageInputActive = true"
+        @blur="messageInputActive = false"
+        v-model="message"
+        :class="messageInputClasses"
+        class="v-contact-form__input v-contact-form__input--textarea"
+        style="resize: none;"
+        name="message"
+        id="message"
+        required
+      ></textarea>
+      <label
+        :class="messageLabelClasses"
+        class="v-contact-form__label u-text-mars-title"
+        for="message"
+      >Message</label>
+    </div>
+    <input type="submit" value="Send" class="v-contact-form__submit">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ContactForm",
+  data() {
+    return {
+      firstNameInputActive: false,
+      lastNameInputActive: false,
+      phoneInputActive: false,
+      emailInputActive: false,
+      messageInputActive: false,
+      firstName: "",
+      lastName: "",
+      phone: "",
+      email: "",
+      message: ""
+    };
+  },
+  computed: {
+    firstNameInputClasses() {
+      return {
+        "v-contact-form__input--active": this.firstNameInputActive
+      };
+    },
+    firstNameLabelClasses() {
+      return {
+        "v-contact-form__label--active":
+          this.firstNameInputActive || this.firstName
+      };
+    },
+    lastNameInputClasses() {
+      return {
+        "v-contact-form__input--active": this.lastNameInputActive
+      };
+    },
+    lastNameLabelClasses() {
+      return {
+        "v-contact-form__label--active":
+          this.lastNameInputActive || this.lastName
+      };
+    },
+    phoneInputClasses() {
+      return {
+        "v-contact-form__input--active": this.phoneInputActive
+      };
+    },
+    phoneLabelClasses() {
+      return {
+        "v-contact-form__label--active": this.phoneInputActive || this.phone
+      };
+    },
+    emailInputClasses() {
+      return {
+        "v-contact-form__input--active": this.emailInputActive
+      };
+    },
+    emailLabelClasses() {
+      return {
+        "v-contact-form__label--active": this.emailInputActive || this.email
+      };
+    },
+    messageInputClasses() {
+      return {
+        "v-contact-form__input--active": this.messageInputActive
+      };
+    },
+    messageLabelClasses() {
+      return {
+        "v-contact-form__label--active": this.messageInputActive || this.message
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.v-contact-form {
+  display: flex;
+  flex-direction: column;
+
+  &__input-group {
+    display: flex;
+  }
+
+  &__input-wrapper {
+    padding: $venus $mercury;
+    position: relative;
+    width: 100%;
+  }
+
+  &__label {
+    left: $mercury;
+    margin-left: $mercury;
+    position: absolute;
+    top: $venus;
+    transition: all 0.1s ease-in;
+
+    &--active {
+      color: $gold;
+      transform: translateY(-100%);
+    }
+  }
+
+  &__input {
+    border: solid inset 0.5px $black;
+    font-family: "Heebo", sans-serif;
+    transition: border 0.1s ease-in;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+
+    &--active {
+      background-color: $grey;
+      border: solid $pluto $gold;
+    }
+
+    &--text {
+      padding-top: $pluto;
+    }
+
+    &--textarea {
+      height: 100px;
+    }
+  }
+
+  &__submit {
+    align-self: center;
+    background-color: $gold;
+    border: none;
+    border-radius: $pluto;
+    color: $white;
+    height: $neptune;
+    text-transform: uppercase;
+    transition: $transition-opacity;
+    width: 30%;
+
+    @include lg {
+      align-self: flex-start;
+    }
+
+    &:hover {
+      opacity: $opacity;
+    }
+  }
+}
+</style>
+
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="v-about v-section">
+  <div class="v-contact v-section">
     <div class="u-margin-bottom-saturn"></div>
-    <div class="v-contact__box">
-      <div v-for="(contactDetail, index) in contactDetails" :key="index">
-        <a class="v-contact__link" target="_blank" :href="contactDetail.href">
-          <i :class="contactDetail.iconClass" class="v-contact__link-icon"></i>
-          <div class="v-contact__link-detail">{{ contactDetail.info }}</div>
-        </a>
+    <div class="v-contact__content">
+      <div class="v-contact__box u-text-venus-alt">
+        <div v-for="(contactDetail, index) in contactDetails" :key="index">
+          <a class="v-contact__link" target="_blank" :href="contactDetail.href">
+            <i :class="contactDetail.iconClass" class="v-contact__link-icon"></i>
+            <div class="v-contact__link-detail">{{ contactDetail.info }}</div>
+          </a>
+        </div>
       </div>
+      <div class="v-contact__divider-mobile"></div>
+
+      <contact-form></contact-form>
     </div>
   </div>
 </template>
 
 <script>
+import ContactForm from "@/components/ContactForm";
+
 export default {
+  components: { ContactForm },
   data() {
     return {
       contactDetails: [
@@ -45,11 +53,38 @@ export default {
 
 <style lang="scss">
 .v-contact {
+  &__content {
+    align-items: center;
+    color: $charcoal;
+    display: flex;
+    flex-direction: column;
+
+    @include lg {
+      align-items: flex-start;
+      flex-direction: row-reverse;
+      justify-content: center;
+    }
+  }
+
+  &__box {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 300px;
+    justify-content: space-between;
+    padding: $venus;
+
+    @include lg {
+      align-items: flex-start;
+      height: 250px;
+      margin-left: $saturn;
+    }
+  }
+
   &__link {
     align-items: center;
     color: $black;
     display: inline-flex;
-    margin-bottom: $venus;
     padding: $mercury;
     text-decoration: none;
     transition: $transition-opacity;
@@ -65,6 +100,17 @@ export default {
 
     &-detail {
       margin-left: $venus;
+    }
+  }
+
+  &__divider-mobile {
+    background-color: $gold;
+    height: $mercury;
+    margin: $uranus 0;
+    width: 30%;
+
+    @include lg {
+      display: none;
     }
   }
 }
