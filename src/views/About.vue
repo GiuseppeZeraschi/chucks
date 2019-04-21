bea4257f55679c6db3c6fbc58e5430ef6f64d9d8
Giuseppe Zeraschi <giuseppezeraschi@gmail.com>
2019-04-21 21:36:03 +0100

    removed tombstoning

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,20 +2,9 @@
   <div class="v-about v-section">
     <div class="u-margin-bottom-saturn"></div>
     <div class="v-about__content">
-      <div v-show="imageLoaded" class="v-about__image-wrapper">
-        <transition name="fade">
-          <div v-show="!imageLoaded" class="v-about__image-tombstone"></div>
-        </transition>
-        <transition name="fade">
-          <img
-            @load="imageLoaded = true"
-            class="v-about__owners-image"
-            src="@/assets/owners.jpeg"
-            alt="Chuck's owners"
-          >
-        </transition>
+      <div class="v-about__image-wrapper">
+        <img class="v-about__owners-image" src="@/assets/owners.jpeg" alt="Chuck's owners">
       </div>
-      <div class="v-about__owners-image-tombstone"></div>
       <div class="v-about__divider-mobile"></div>
       <div class="v-about__text-wrapper">
         <div>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor, tenetur maiores vero illum architecto aut magni repellat voluptatum numquam aspernatur culpa dolore beatae vitae eos facilis, distinctio consequuntur amet sequi est. Quo nisi quos harum quasi mollitia laudantium, reprehenderit quidem! Qui totam ipsum non ea ipsam, id vitae, praesentium accusantium quae, nisi veniam inventore consequuntur expedita reiciendis eligendi enim?"</div>
@@ -24,16 +13,6 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      imageLoaded: false
-    };
-  }
-};
-</script>
 
 <style lang="scss">
 .v-about {
@@ -53,26 +32,13 @@ export default {
   }
 
   &__image-wrapper {
-    height: 400px;
-    position: relative;
-    width: 400px;
-  }
-
-  &__image-tombstone {
-    background-color: $grey;
-    border-radius: $mercury;
-    height: 400px;
-    position: absolute;
-    width: 400px;
+    max-width: 400px;
+    max-height: 400px;
   }
 
   &__owners-image {
     border-radius: $mercury;
     box-shadow: $box-shadow;
-    height: 400px;
-    position: absolute;
-    width: 400px;
-    z-index: 10;
   }
 
   &__divider-mobile {
